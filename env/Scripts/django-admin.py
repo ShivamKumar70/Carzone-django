4eeab910201655696f6d6f39de9a45c067b08de1
Shivam <skumar975688@gmail.com>
2021-06-25 13:16:17 +0530

    Carzone files 1st commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lenobo\dropbox\my pc (laptop-htmt1ifn)\desktop\carzone\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
